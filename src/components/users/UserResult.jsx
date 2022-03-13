6e4d7266d83bf122efdc17f76384da0a4a40603e
mfkfawas <mfkfawas@gmail.com>
2022-03-13 19:17:05 +0530

    display users

--- src/components/users/UserResult.jsx
+++ src/components/users/UserResult.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Spinner from '../layout/Spinner';
+import UserItem from './UserItem';
 
 const UserResult = () => {
   const [users, setUsers] = useState([]);
@@ -20,6 +21,7 @@ const UserResult = () => {
     );
 
     const data = await response.json();
+    console.log(data);
     setUsers(data);
     setLoading(false);
   };
@@ -28,7 +30,7 @@ const UserResult = () => {
     return (
       <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
         {users.map(user => (
-          <h3>{user.login}</h3>
+          <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
